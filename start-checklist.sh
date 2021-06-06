b421388eda0c135e59bf2b5f949ffac98edeaccd
webcipher101 <51315763+webcipher101@users.noreply.github.com>
2021-06-06 21:02:35 +0000

    Update start-checklist.sh

--- start-checklist.sh
+++ start-checklist.sh
@@ -3,4 +3,6 @@
 figlet -f slant.tlf "#recon-checklist"
 echo -e "\e[33m by: @ WebCipher101"
 
-sensible-browser /home/webcipher/checklist/index.html
+mypwd=${PWD}
+
+sensible-browser $mypwd/index.html
